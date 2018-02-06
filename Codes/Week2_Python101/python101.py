c955b098854df8283afed8c98f8426c27cd88ffb
hy-lin <h.lin@psychologie.uzh.ch>
2018-02-06 18:26:13 +0100

    adding documents and start writing python 101

---
+++ Codes/Week2_Python101/python101.py
@@ -0,0 +1,27 @@
+# first and foremost
+'Hello World'
+
+# using python as calculator
+3 * 4
+832 + 382
+12 / 3
+13 % 3
+13 // 3
+1 / 2
+
+# let python remembering the outcome
+a = 3 * 4
+b = 832 + 382
+c = a + b
+c = c + 3
+c += 3
+
+s = 'Hello World'
+
+# creating and accessing a list
+d = [a, b, c, s]
+d[2]
+range(3) #help(range)
+e = [a, b, c, d]
+e[3][2]
+
