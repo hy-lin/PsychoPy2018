a9f2c31bacb6d839a3a27a9927041bbbb76c00b2
hy-lin <h.lin@psychologie.uzh.ch>
2018-02-18 16:30:51 +0100

    finished python101

--- Codes/Week2_Python101/python101.py
+++ Codes/Week2_Python101/python101.py
@@ -1,27 +1,51 @@
 # first and foremost
 'Hello World'
-
-# using python as calculator
-3 * 4
-832 + 382
-12 / 3
-13 % 3
-13 // 3
-1 / 2
+print('Welcome to the classic memory experiment.')
 
 # let python remembering the outcome
-a = 3 * 4
-b = 832 + 382
-c = a + b
-c = c + 3
-c += 3
+setsize = 6
+display_duration = 500  # in ms.
+display_duration = display_duration / 1000  # in s.
 
-s = 'Hello World'
+stimuli = ['bent',
+           'cart',
+           'line',
+           'birth',
+           'ultra',
+           'excuse']
 
-# creating and accessing a list
-d = [a, b, c, s]
-d[2]
-range(3) #help(range)
-e = [a, b, c, d]
-e[3][2]
+probe_type = 'old'
+serial_position = 3  # tbd after the probe type is set
+probe = ''  # tbd after the probe type is set
 
+probe, serial_position = setupProbe(stimuli, probe_type, serial_position)
+
+# let's run it!
+
+trial_index = 1
+print('Beginning trial {}'.format(trial_index))
+# print('Beginning trial', trial_index)
+
+for i in range(setsize):
+    print(stimuli[i])
+
+print('*')
+
+print(probe)
+response = raw_input('o/n?')
+
+
+def setupProbe(stimuli, probe_type=None, serial_position=None):
+    # this function determines the content of the probe
+
+    if probe_type is None:
+        probe_type = 'old'
+
+    # fill in serial_position here
+
+    if probe_type == 'new':
+        probe = 'dogs'
+    else:
+        probe = stimuli[serial_position - 1]
+
+    return probe, serial_position
